94511a5a7773cc463ea6ea8ca98ee85da4acc45f
monki <edgarandres29@gmail.com>
2012-03-21 15:01:07 -0500

    Iniciando

---
+++ Taller1/ReadImage.cpp
@@ -0,0 +1,18 @@
+/*
+ * ReadImage.cpp
+ *
+ *  Created on: 21/03/2012
+ *      Author: monki
+ */
+
+#include "ReadImage.h"
+
+ReadImage::ReadImage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ReadImage::~ReadImage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Taller1/ReadImage.h
@@ -0,0 +1,17 @@
+/*
+ * ReadImage.h
+ *
+ *  Created on: 21/03/2012
+ *      Author: monki
+ */
+
+#ifndef READIMAGE_H_
+#define READIMAGE_H_
+
+class ReadImage {
+public:
+	ReadImage();
+	virtual ~ReadImage();
+};
+
+#endif /* READIMAGE_H_ */
